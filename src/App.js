372908bb0372654eb2a8ddf7d9e77f209856b1ab
mike smirnov <migel1976@gmail.com>
2022-11-03 12:36:39 +0300

    navbar has done

--- src/App.js
+++ src/App.js
@@ -2,20 +2,19 @@ import React from 'react'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import About from './pages/About'
 import Posts from './pages/Posts'
+import Error from './pages/Error'
+
+import MyNavbar from './components/UI/navbar/MyNavbar'
 
 export function App(){
 	return(
 		<BrowserRouter>
-			<Routes>
-				{/* <div className='navbar'> */}
-				{/* 	<div className='navbar__links'> */}
-				{/* 		<a href='/about'>About</a> */}
-				{/* 		<a href='/posts'>Posts</a> */}
-				{/* 	</div> */}
-				{/* </div> */}
-				<Route path='/about' element={<About />} />
-				<Route path='/posts' element={<Posts />} />
-			</Routes>
+			<MyNavbar />
+				<Routes>
+					<Route exact path='/about' element={<About />} />
+					<Route exact path='/posts' element={<Posts />} />
+					<Route path='*' element={<Error />} />
+				</Routes>
 		</BrowserRouter>
 	)
 }
